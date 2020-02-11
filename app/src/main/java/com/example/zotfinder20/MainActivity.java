60660b189dd0fa58a7401352b8e2d130d7186076
Your Name <you@example.com>
2020-02-10 23:04:17 -0800

    Modified the main menu

--- app/src/main/java/com/example/zotfinder20/MainActivity.java
+++ app/src/main/java/com/example/zotfinder20/MainActivity.java
@@ -12,30 +12,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button button_find_a_class;
     private Button button_find_a_building;
     private Button button_find_a_room;
-    private Button button_find_other;
+    private Button button_help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button_find_a_class = findViewById(R.id.classroom_button);
+        button_find_a_class = findViewById(R.id.outdoor_classroom_button);
         button_find_a_building = findViewById(R.id.building_button);
-        button_find_a_room = findViewById(R.id.room_button);
-        button_find_other = findViewById(R.id.other_button);
+        button_find_a_room = findViewById(R.id.indoor_classroom_button);
+        button_help = findViewById(R.id.help_button);
 
         button_find_a_class.setOnClickListener(this);
         button_find_a_building.setOnClickListener(this);
         button_find_a_room.setOnClickListener(this);
-        button_find_other.setOnClickListener(this);
-        //ADD THE REST
+        button_help.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId())
         {
-            case R.id.classroom_button:
+            case R.id.outdoor_classroom_button:
                 Intent intent_selected_classroom = new Intent(this, selected_classroom.class);
                 startActivity(intent_selected_classroom);
                 finish();
@@ -45,14 +44,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent_selected_building);
                 finish();
                 break;
-            case R.id.room_button:
+            case R.id.indoor_classroom_button:
                 Intent intent_selected_room = new Intent(this,selected_room.class);
                 startActivity(intent_selected_room);
                 finish();
                 break;
-            case R.id.other_button:
-                Intent intent_selected_other = new Intent(this,selected_other.class);
-                startActivity(intent_selected_other);
+            case R.id.help_button:
+                Intent intent_selected_help = new Intent(this,selected_other.class);
+                startActivity(intent_selected_help);
                 finish();
                 break;
 
